56294e907d1e8908a61d9a8aa083447fe819486c
MarijaJ14 <148470774+MarijaJ14@users.noreply.github.com>
2024-01-17 19:45:06 +0000

    Update test.py

--- test.py
+++ test.py
@@ -17,9 +17,11 @@ print("Output Text:", pre_processing_instance.output_text)
 # Test Linguistic Analysis
 linguistic_analysis_instance = Linguistic_Analysis(pre_processing_instance.output_text)
 pos_tags = linguistic_analysis_instance.disambiguate_pos(pre_processing_instance.output_text)
+phrases = linguistic_analysis_instance.syntatic_analysis(pos_tags, pre_processing_instance.output_text )
 
 print("\nLinguistic Analysis Output:")
 print("Input Text:", input_text)
 print("POS Tags:", pos_tags)
+print("Phrases:", phrases)
 
 
